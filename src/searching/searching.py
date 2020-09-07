1ff2d26268843ec2c3f7519820d6c8c7a6ea06a7
tevin davis <aod.td1105@gmail.com>
2020-09-06 20:38:14 -0400

    Finsihed MVP for this project

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,7 @@
 def linear_search(arr, target):
-    # Your code here
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
 
     return -1   # not found
@@ -9,6 +11,18 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    start = 0
+    end = (len(arr) - 1)
+    while end >= start:
+        mid_index = (start + end) // 2
+        if arr[mid_index] == target:
+            return mid_index
+        else:
+            if arr[mid_index] > target:
+                end = mid_index - 1
+            if arr[mid_index] < target:
+                start = mid_index + 1 
+
 
 
     return -1  # not found
